a232e655c8baebeb11870b1397291dee50af8af7
Facundo Arimany <arimanyfacundo@gmail.com>
2023-04-20 17:05:50 +0200

    Exercise 6 finished

--- ArrayCardio.js
+++ ArrayCardio.js
@@ -53,9 +53,59 @@
       },0);
       console.table(sumYearsLivedTogether)
     // 5. Sort the inventors by years lived
-
+    inventors.sort((inventor2, inventor)=> {
+      return (inventor.passed - inventor.year) - (inventor2.passed-inventor2.year)
+    })
+    console.table(inventors)
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+      const parisBoulevards = [
+        "Boulevards of Paris",
+        "City walls of Paris",
+        "Thiers wall",
+        "Wall of Charles V",
+        "Wall of Philip II Augustus",
+        "City gates of Paris",
+        "Haussmann's renovation of Paris",
+        "Boulevards of the Marshals",
+        "Boulevard Auguste-Blanqui",
+        "Boulevard Barbès",
+        "Boulevard Beaumarchais",
+        "Boulevard de l'Amiral-Bruix",
+        "Boulevard Mortier",
+        "Boulevard Poniatowski",
+        "Boulevard Soult",
+        "Boulevard des Capucines",
+        "Boulevard de la Chapelle",
+        "Boulevard de Clichy",
+        "Boulevard du Crime",
+        "Boulevard du Général-d'Armée-Jean-Simon",
+        "Boulevard Haussmann",
+        "Boulevard de l'Hôpital",
+        "Boulevard des Italiens",
+        "Boulevard Lefebvre",
+        "Boulevard de la Madeleine",
+        "Boulevard de Magenta",
+        "Boulevard Malesherbes",
+        "Boulevard Marguerite-de-Rochechouart",
+        "Boulevard Montmartre",
+        "Boulevard du Montparnasse",
+        "Boulevard Raspail",
+        "Boulevard Richard-Lenoir",
+        "Boulevard Saint-Germain",
+        "Boulevard Saint-Michel",
+        "Boulevard de Sébastopol",
+        "Boulevard de Strasbourg",
+        "Boulevard du Temple",
+        "Boulevard Voltaire",
+        "Boulevard de la Zone",
+      ]
+       const boulevardsConDE = parisBoulevards.filter((boulevard)=>{
+          return boulevard.includes("de")
+        })
+        
+
+        console.log(boulevardsConDE)
 
 
     // 7. sort Exercise
